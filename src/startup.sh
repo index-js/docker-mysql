f5372a9d2ca62f620d79a270357004564088c0f9
Yanglin <i@yangl.in>
2018-12-17 21:48:34 +0800

    Builder

--- src/startup.sh
+++ src/startup.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+# https://wiki.alpinelinux.org/wiki/MariaDB
 
 [[ "$MYSQL_ROOT_PASSWORD" ]] && {
 
 cat > /tmpfile << EOF
-  DROP DATABASE test;
   FLUSH PRIVILEGES;
   GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
   GRANT ALL ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
   GRANT ALL ON *.* TO 'root'@'::1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
   GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
   DELETE FROM mysql.user WHERE User='';
+  DROP DATABASE test;
 EOF
 
   mysqld --user=root --bootstrap < /tmpfile
   rm -rf /tmpfile
 }
 
-mysqld_safe --user=root
+mysqld_safe --user=root && crond -f
