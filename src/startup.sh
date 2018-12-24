f2145c03bd7eebfc0d79a628f878af75dbc6a71e
Yanglin <i@yangl.in>
2018-12-24 16:24:47 +0800

    Perfect

--- src/startup.sh
+++ src/startup.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 # https://wiki.alpinelinux.org/wiki/MariaDB
 
-mysql_install_db > /dev/null
+if [ -d /var/lib/mysql ]; then
+  echo '[i] MySQL directory already present, skipping creation'
+else
+  mysql_install_db > /dev/null
+  [[ "$MYSQL_ROOT_PASSWORD" ]] && {
 
-[[ "$MYSQL_ROOT_PASSWORD" ]] && {
-
-cat > /tmpfile << EOF
-  FLUSH PRIVILEGES;
-  GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-  GRANT ALL ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-  GRANT ALL ON *.* TO 'root'@'::1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-  GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-  DELETE FROM mysql.user WHERE User='';
-  DROP DATABASE test;
+    cat > /tmpfile << EOF
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'::1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
 EOF
-
-  mysqld --user=root --bootstrap < /tmpfile
-  rm -rf /tmpfile
-}
+    mysqld --user=root --bootstrap < /tmpfile
+    rm -rf /tmpfile
+  }
+fi
 
 mysqld_safe --user=root && crond -f
